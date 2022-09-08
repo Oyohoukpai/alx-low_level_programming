f441578060d3cbc3de77c246eac6ae341f59b1ba
Oyohoukpai <oyohoukpai@gmail.com>
2022-09-08 16:06:21 -0700

    Hello world

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#!/bin/bash
+/**
+ * main - print the string in the put function
+ * 
+ * descreption: using the main function
+ * this program prints "Programming is like building a multilingual puzzle"
+ * Return: 0
+ */
+int main(void)
+{
+	put("\"Programming is like building a multilingual puzzle");
+	return(0)
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#!/bin/bash
+#include <stdio.h>
+/**
+ * main - print the string in the put function
+ * Description: using the main function
+ * this program prints "with proper grammer, but the outcome is like a piece of art"
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is like a piece of art,\n");
+	return (0);
+}
