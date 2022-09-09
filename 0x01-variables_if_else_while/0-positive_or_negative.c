906f8a1f69de6dcef9c92e734309258c0e49fba1
Oyohoukpai <oyohoukpai@gmail.com>
2022-09-09 15:02:42 -0700

    Task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,23 +6,37 @@
  * Description: prints if a number is positive or nagetive.
  * Return: 0 made me do
  */
+
 int main(void)
+
 {
 	int n;
 
 	srand(time(0));
+	
 	n = rand() - RAND_MAX / 2;
+	
 	if (n > 0)
+	
 	{
 		printf("%d is positive\n", n);
+	
 	}
+	
 	else if (n == 0)
+	
 	{
+	
 		printf("%d is zero\n", n);
+	
 	}
+	
 	else
+	
 	{
+	
 		printf("%d is nagetive\n", n);
 	}
+	
 	return (0);
 }
