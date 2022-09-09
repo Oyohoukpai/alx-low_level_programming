488a3cf84406f5b33d08f7cbf226088812bdb88c
Oyohoukpai <oyohoukpai@gmail.com>
2022-09-08 20:03:57 -0700

    taSk 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
-/*
- *
- * main - main block
- * Description: use printf
- * Return: Aiways 0 (Success)
- *
- */
+/**
+*main - Betty
+*Description: use printf
+*Return: Aiways 0 (Success)
+*/
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
