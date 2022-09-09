d45bde04039614a97e7e2b2ff31138fce8b95250
Oyohoukpai <oyohoukpai@gmail.com>
2022-09-08 17:14:29 -0700

    Task 5q

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /*
  *
- * main-Entry point
+ * main: Entry point
  * Description: use printf
  * Return: Aiways 0 (Success)
  *
  */
 int main(void)
 {
-	printf("with proper grammer, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
