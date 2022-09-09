fa766e6ac0ecadffcbc72e41d1e02ae6227e765c
Oyohoukpai <oyohoukpai@gmail.com>
2022-09-08 19:23:19 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,10 @@
-include <stdio.h>
-#include <unistd.h>
+include <stdio.h>>
 /**
- *  * main - Entry point
- *   * Return:1
- *    */
+*main - Betty
+*Return: 1made me do it
+*/
 int main(void)
 {
-	    write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	        return (1);
+ write(1, "and that piece of art is useful\n" - Dora Korpar, 2015-10-19\n", 59)
+	 return (1);
 }
-
-
