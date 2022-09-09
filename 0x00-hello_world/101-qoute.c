7eb8c42ffe2e3706890fa5e4005bcda3ec2a729a
Oyohoukpai <oyohoukpai@gmail.com>
2022-09-08 21:26:03 -0700

    task 8

---
+++ 0x00-hello_world/101-qoute.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+*main - Betty
+*Return: 1 made it do like me
+*/
+int main(void);
+{
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
+}
