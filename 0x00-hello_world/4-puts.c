b8748fc34a5e3bd743ed80cea24a169ea7aec1e9
Oyohoukpai <oyohoukpai@gmail.com>
2022-09-08 18:29:41 -0700

    TASK 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- *main - Betty
- *Return: 0 mad me do it 
- */
+*main - Betty
+*Return: 0 mad me do it 
+*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
