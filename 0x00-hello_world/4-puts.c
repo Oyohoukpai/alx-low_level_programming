f5df33565405bf32401c8974ef65e89798fa3be8
Oyohoukpai <oyohoukpai@gmail.com>
2022-09-08 17:29:53 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,21 @@
 #include <stdio.h>
-/*
- * main - print the string in the put function
- * 
- * descreption: using the main function
- * this program prints "Programming is like building a multilingual puzzle
- * Return: 0
- */
+
+/**
+ *
+ *  * main - main block
+ *
+ *   * Return: 0
+ *
+ *    */
+
 int main(void)
+
 {
-	put("\"Programming is like building a multilingual puzzle");
-	return(0)
+
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+
 }
+
+
