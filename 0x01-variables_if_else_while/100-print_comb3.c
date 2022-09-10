d7f867d3772c25d2c86f4a3a9b046be74f70d155
Oyohoukpai <oyohoukpai@gmail.com>
2022-09-10 08:06:34 -0700

    task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -4,30 +4,61 @@
 * Return: Always 0 (Success)
 */
 int main(void)
-{
-int i, e;
 
-i = 48;
-e = 48;
-while (e < 58)
 {
-i = 48;
-while (i < 58)
-{
-if (e != i && e < i)
-{
-putchar(e);
-putchar(i);
-if (i == 57 && e == 56)
-{
-break;
-}
-putchar(',');
-putchar(' ');
-}
-i++;
-}
-e++;
-putchar('\n');
-return (0);
+
+		int i, e;
+
+
+
+			i = 48;
+
+				e = 48;
+
+
+
+					while (e < 58)
+
+							{
+
+										i = 48;
+
+												while (i < 58)
+
+															{
+
+																			if (e != i && e < i)
+
+																							{
+
+																												putchar(e);
+
+																																putchar(i);
+
+																																				if (i == 57 && e == 56)
+
+																																									{
+
+																																															break;
+
+																																																			}
+
+																																								putchar(',');
+
+																																												putchar(' ');
+
+																																															}
+
+																						i++;
+
+																								}
+
+														e++;
+
+															}
+
+						putchar('\n');
+
+							return (0);
+
 }
