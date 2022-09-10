bd1c2e42a463bc8006d0ff995722bfa281e7f451
Oyohoukpai <oyohoukpai@gmail.com>
2022-09-10 03:36:32 -0700

    task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,27 +2,27 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- * main - Entry point
- * Description: prints if a number is positive or nagetive.
- * Return: 0 made me do
- */
+* main - Entry point
+* Description: prints if a number is positive or nagetive.
+* Return: Always 0 (success)
+*/
 int main(void)
 {
 	int n;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if (n > 0)
-	{
-		printf("%d is positive\n", n);
-	}
-	else if (n == 0)
-	{
-  	printf("%d is zero\n", n);
-	}
-	else
-	{
-		printf("%d is nagetive\n", n);
-	}
-	return (0);
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n > 0)
+{
+printf("%d is positive\n", n);
+}
+else if (n == 0)
+{
+printf("%d is zero\n", n);
+}
+else
+{
+printf("%d is nagetive\n", n);
+}
+return (0);
 }
