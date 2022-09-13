8596d23adfabf1de58bc23f5b7e63e1ba0ffe6a7
Oyohoukpai <oyohoukpai@gmail.com>
2022-09-13 10:24:55 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Description: Write a function that prints the alphabet in lowercase.
+ * Return: Always a - z (success)
+ */
+void print_alphabet(void);
+{
+int n;
+
+n = 'a';
+
+{
+for (n = 'a'; n <= 'z'; n++)
+_putchar(n);
+}
+_putchar('\n');
+return (a - z);
+}
