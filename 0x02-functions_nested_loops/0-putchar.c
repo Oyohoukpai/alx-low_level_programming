2b57635b5d1a4ccc75f497c09d8d772a9d1d4b74
Oyohoukpai <oyohoukpai@gmail.com>
2022-09-13 09:51:22 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,16 +4,15 @@
  * Description: Write a program that prints _putchar, followed by a new line.
  * Return: Always 0 (success)
  */
-int _putchar(void)
 int main(void)
 {
-	char *_putchar = main;
+	char str[] = "_putchar";
+	int ch;
 
 
-	while (*_putchar)
+	for (ch = 0; ch < 8; ++ch)
 	{
-		_putchar('_putchar');
-	_putchar++;
+		_putchar(str[ch]);
 }
 _putchar('\n');
 return (0);
