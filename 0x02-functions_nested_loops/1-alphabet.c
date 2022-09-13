0fca400c50ee8ee1a17eb9731034a603c639c72b
Oyohoukpai <oyohoukpai@gmail.com>
2022-09-13 14:09:08 -0700

    task 2

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,15 @@
 #include "main.h"
 /**
-*Print_alphabet - Entry point
- *Description: Program that prints the alphabet in lowercase
- *follow with a new line
- *Return: Exit int function with an int 0
+ * Description: program that prints the alphabets
+ * followed with a new line
+ * Return: Exit int function with an int 0
  */
 void print_alphabet(void)
 {
 int ny;
-
 for (ny = 'a'; ny <= 'z'; ny++)
 {
-	_putchar(ny);
+_putchar(ny);
 }
 _putchar('\n');
 }
--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,20 +1,15 @@
 #include "main.h"
 /**
-* print_alphabet_x10 - a function that prints 10 times the alphabet
-* Return: x10 a-z
+*print_alphabet_x10 - a function that prints 10 times the alphabet
+*Return: x10 a-z
 */
 void print_alphabet_x10(void)
 {
-
 int n, co;
-
-
 co = 0;
 
-
 while (co < 10)
-
-{						
+{
 for (n = 'a'; n <= 'z'; n++)
 {
 _putchar(n);
