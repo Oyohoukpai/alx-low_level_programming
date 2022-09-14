63ad77a29b88a9e03a862e13720e1b00efac699c
Oyohoukpai <oyohoukpai@gmail.com>
2022-09-13 18:54:27 -0700

    Task 14

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -10,14 +10,14 @@ int main(void)
 
 	for (count = 0; count < 50; count++)
 	{
-		sum fib1 + fib2;
+		sum = fib1 + fib2;
 		printf("%lu", sum);
 
 		fib1 = fib2;
 		fib2 = sum;
 
 		if (count == 49)
-			print("\n");
+			printf("\n");
 		else
 			printf(", ");
 	}
