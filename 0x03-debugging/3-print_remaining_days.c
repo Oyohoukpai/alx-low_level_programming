87bac9560596d6f433a86011a1155541fc7775e3
Oyohoukpai <oyohoukpai@gmail.com>
2022-09-14 21:00:03 -0700

    task 3

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 #include "main.h"
 /**
  * print_remaining_days - takes a date and prints how many days are
@@ -10,7 +9,6 @@
  * Return: void
  */
 void print_remaining_days(int month, int day, int year)
-
 {
 if ((year % 4 == 0 || year % 400 == 0) && !(year % 100 == 0))
 {
