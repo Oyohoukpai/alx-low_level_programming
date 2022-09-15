d8f9f09efab5d0d39ef423c620c2efb77b667da5
Oyohoukpai <oyohoukpai@gmail.com>
2022-09-14 20:22:02 -0700

    task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,13 +1,14 @@
 #include "main.h"
 /**
- *main - Debugging
- *Return: 0
+ * main - Debugging
+ * Return: 0
  */
 int main(void)
 {
-int i;
+	int i;
 
-i = 0;
-positive_or_negative(i);
-return (0);
+	i = 0;
+
+	positive_or_negative(i);
+	return (0);
 }
