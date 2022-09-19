cb5141ac7c4c9afe80b318d4eee1054dc857fffb
Oyohoukpai <oyohoukpai@gmail.com>
2022-09-19 16:03:12 -0700

    My tasks 0 - 5

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - This takes a pointer to a parameter and updates its value
+ *
+ * @n: enter an integer
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+
+	*n = 98;
+}
