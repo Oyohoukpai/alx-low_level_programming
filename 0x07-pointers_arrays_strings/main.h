c8dab386665361256302487c4b466b5a0d56b057
Oyohoukpai <oyohoukpai@gmail.com>
2022-09-26 13:31:14 -0700

    main.h

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,12 +1,7 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
 #ifndef MAIN_H
 #define MAIN_H
-
-/**
- * File: holberton.h
- * Auth: Brennan D Baraban
- * Desc: Header file containing prototypes for all functions
- *       used in the 0x06-pointers_arrays_strings directory.
- */
 
 #include <stddef.h>
 
