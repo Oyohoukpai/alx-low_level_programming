7d5c960c028cf21727646705b60520a97a5abcfb
Oyohoukpai <oyohoukpai@gmail.com>
2022-10-20 14:39:59 -0700

    tasks

---
+++ 0x12-singly_linked_lists/hello_holberton.c
@@ -0,0 +1,17 @@
+// from the buttom prints... - Oyohoukpai
+
+SECTION .data
+msg:	db "Hello, Holberton", 0
+fmt:	db "%s", 10, 0
+
+	SECTION .text
+	extern printf
+	global main
+main:
+	mov esi, msg
+	mov edi, fmt
+	mov eax, 0
+	call printf
+
+	mov eax, 0
+	ret
